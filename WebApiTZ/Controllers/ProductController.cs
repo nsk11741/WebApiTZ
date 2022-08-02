f37e9e2579535e61242dc3028324ff7ac7c2bedd
Nikolay N <n68721@outlook.com>
2022-08-02 17:40:49 +0400

    Add: Nlog, save JSON to SQLite

--- WebApiTZ/Controllers/ProductController.cs
+++ WebApiTZ/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using WebApiTZ.Models;
 
 namespace WebApiTZ.Controllers
@@ -9,13 +11,16 @@ namespace WebApiTZ.Controllers
 	[ApiController]
 	public class ProductController : ControllerBase
 	{
+		private readonly ILogger<ProductController> _logger;
 		public FilterModel Filter { get; set; }
 		public IEnumerable<ProductModel> Products { get; private set; }
 
 		private readonly ProductService _service;
-		public ProductController(ProductService service)
+		public ProductController(ProductService service, ILogger<ProductController> logger)
 		{
 			_service = service;
+			_logger = logger;
+			_logger.LogDebug(1, "NLog injected into HomeController");
 		}
 
 		[HttpGet("readFilter")]
@@ -31,6 +36,9 @@ namespace WebApiTZ.Controllers
 			{
 				return NotFound();
 			}
+
+			_logger.LogInformation(JsonSerializer.Serialize(Products));
+
 			return new JsonResult(Products);
 		}
 
@@ -43,6 +51,8 @@ namespace WebApiTZ.Controllers
 				return BadRequest(ModelState);
 			}
 			await _service.CreateProducts(Inputs);
+
+			_logger.LogInformation(JsonSerializer.Serialize(Inputs));
 
 			return Ok();
 
