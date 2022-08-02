4ea9151159e44eaea7066ceb727ec9c6b77c8f05
Nikolay N <n68721@outlook.com>
2022-08-02 19:43:51 +0400

    Add: EnsureDB

--- WebApiTZ/Program.cs
+++ WebApiTZ/Program.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Configuration;
 
 namespace WebApiTZ
 {
@@ -15,6 +17,8 @@ namespace WebApiTZ
 	{
 		public static void Main(string[] args)
 		{
+			EnsureDB();
+
 			var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
 			try
@@ -33,6 +37,22 @@ namespace WebApiTZ
 				// Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
 				NLog.LogManager.Shutdown();
 			}
+		}
+
+		static void EnsureDB()
+		{
+			if (System.IO.File.Exists(@"C:\temp\logs.db"))
+				return;
+
+			using (SQLiteConnection connection = new SQLiteConnection(@"Data Source=C:\temp\logs.db"))
+			using (SQLiteCommand command = new SQLiteCommand(
+				"CREATE TABLE logging (longdate TEXT, level TEXT, logger TEXT, message TEXT, stacktrace TEXT)",
+				connection))
+			{
+				connection.Open();
+				command.ExecuteNonQuery();
+			}
+
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
